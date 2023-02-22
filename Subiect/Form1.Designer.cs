b1d3851334ba72ecac7f708be0b6fb2dc650d53a
ionutzzzzz <67456260+ionutzzzzz@users.noreply.github.com>
2023-02-22 20:41:26 +0200

    Update Project

--- Subiect/Form1.Designer.cs
+++ Subiect/Form1.Designer.cs
@@ -59,8 +59,9 @@
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(152, 53);
 			this.button1.TabIndex = 2;
-			this.button1.Text = "button1";
+			this.button1.Text = "Logare";
 			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
 			// button2
 			// 
@@ -68,8 +69,9 @@
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(152, 53);
 			this.button2.TabIndex = 3;
-			this.button2.Text = "button2";
+			this.button2.Text = "Inregistrare";
 			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
 			// 
 			// Form1
 			// 
--- Subiect/Form1.cs
+++ Subiect/Form1.cs
@@ -1,15 +1,36 @@
+using System.Data.SqlClient;
+
 namespace Subiect
 {
 	public partial class Form1 : Form
 	{
+		public static string path = Application.ExecutablePath;
+		public static string bazadedate = path + "";
+
 		public Form1()
 		{
 			InitializeComponent();
+			Update();
+		}
+
+		void Update()
+		{
+
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			 
+			
+		}
+
+		private void button1_Click(object sender, EventArgs e) // Logare
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e) // Inregistrare
+		{
+
 		}
 	}
 }
