76de1347be53d77a448b961b0b3b415f0cb201ac
ionutzzzzz <67456260+ionutzzzzz@users.noreply.github.com>
2023-02-26 17:28:14 +0200

    Update Project

--- Subiect/Form3.cs
+++ Subiect/Form3.cs
@@ -25,14 +25,29 @@ namespace Subiect
 
         private void button1_Click(object sender, EventArgs e) // Logare
         {
-            SqlConnection conn = new SqlConnection(Form1.bazadedate);
-            SqlCommand cmd = new SqlCommand();
-
             try
             {
+                SqlConnection conn = new SqlConnection(Form1.bazadedate);
                 conn.Open();
-                cmd.Connection = conn;
-                cmd.CommandText = @"SELECT ";
+
+                SqlDataAdapter sda = new SqlDataAdapter(@"Select email,parola from utilizatori where email='" + textBox1.Text + "' and parola ='" + textBox2.Text + "'", conn);
+                DataTable dt = new DataTable();
+               
+                sda.Fill(dt);
+
+                if(dt.Rows.Count > 0 ) 
+                {
+                    this.Hide();
+                    Form4 f = new Form4();
+                    this.Hide();
+                    f.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Eroare autentificare!");
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
 
                 conn.Close();
             }
